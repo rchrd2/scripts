3ee2b215a64e65fc52aefb9672c3ab62f483874d
Richard Caceres <rchrdccrs@gmail.com>
2022-04-08 13:31:26 -0700

    Cleanup

--- prepare-audio-for-upload.js
+++ prepare-audio-for-upload.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
 
-const { log, c, cFile, funcFile, helpersConfig, readStdIn } = require("./helpers.js");
+const { cFile, helpersConfig, readStdIn } = require("./helpers.js");
 
 helpersConfig.dryRun = false;
 
 var argv = require("minimist")(process.argv.slice(2));
 
-
 const processFile = (f, index, array) => {
-    // log(f);
-    if (f.toLowerCase().endsWith(".wav")) {
-      let originalF = f;
-      f = f.replace(/\.wav$/ig, ".mp3");
-      cFile(`ffmpeg -i '${originalF}' -ab "320k" '${f}'`, f);
-    }
-    cFile(`audiowaveform -i '${f}' -o '${f}.dat' -z 256 -b 8`, `${f}.dat`);
-    cFile(
-      `audiowaveform -i '${f}' -o '${f}.png' --zoom=auto -w 1000 -h 300`,
-      `${f}.png`
-    );
-}
-
+  // log(f);
+  if (f.toLowerCase().endsWith(".wav")) {
+    let originalF = f;
+    f = f.replace(/\.wav$/gi, ".mp3");
+    cFile(`ffmpeg -i '${originalF}' -ab "320k" '${f}'`, f);
+  }
+  cFile(`audiowaveform -i '${f}' -o '${f}.dat' -z 256 -b 8`, `${f}.dat`);
+  cFile(
+    `audiowaveform -i '${f}' -o '${f}.png' --zoom=auto -w 1000 -h 300`,
+    `${f}.png`
+  );
+};
 
 if (require.main === module) {
   [...readStdIn(), ...argv["_"]].forEach(processFile);
