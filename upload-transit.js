d7178c823314fb4d78e55d7757cbdf462e8930ae
Richard Caceres <rchrdccrs@gmail.com>
2023-05-19 16:37:16 -0700

    Bring in some older scripts. Get ready for port to swift

---
+++ upload-transit.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const { c, helpersConfig, pipeable } = require("./helpers.js");
+
+helpersConfig.dryRun = false;
+
+const uploadUrl = "rcaceres@dev.rchrd.net:sites/net.rchrd.dev/web/transit/";
+
+const processFile = (f, index, array) => {
+  // c(`scp '${f}' ${uploadUrl}`);
+  c(`rsync -vv '${f}' ${uploadUrl}`);
+};
+
+if (require.main === module) {
+  pipeable(processFile, [
+    ".wav",
+    ".mp3",
+    ".png",
+    ".dat",
+    ".mov",
+    ".jpg",
+    ".tif",
+    ".aif",
+    ".mp4",
+    ".nef",
+    ".zip",
+  ]);
+}
+
+module.exports = {
+  processFile,
+};
+
+/*
+Note this is scripted with an automator action located
+in the folder /Users/richard/Library/Services/
+*/
